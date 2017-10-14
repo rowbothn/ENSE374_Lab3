bd3839b99d37cd17f6c573860becf5010ca90a3e
Noah Rowbotham <noah.r@sasktel.net>
2017-10-14 17:41:25 -0600

    Added a constructor for LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -8,6 +8,14 @@ import java.util.*;
 */
 
 public class LinkedList {
+	private ListElement begin;
+	int length;
+	
+	public LinkedList() {
+		this.begin = null;
+		this.length = 0;
+	}
+	
 	public void addElement(ListElement le) {
 		
 	}
