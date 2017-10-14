b17d5cf156c2f4d9667c1d256ed5004503f593c2
Noah Rowbotham <noah.r@sasktel.net>
2017-10-14 16:47:51 -0600

    Built the class.

---
+++ ExampleList.java
@@ -0,0 +1,26 @@
+/*
+*
+* Filename: ExampleList.java
+* Author: Noah Rowbotham (200364559)
+*
+*/
+
+public class ListElement {
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement() {
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+}
