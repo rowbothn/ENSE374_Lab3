3ea577948a19701a88b7d023deff66f77e9472ea
Noah Rowbotham <noah.r@sasktel.net>
2017-10-14 16:53:15 -0600

    Renamed ExampleList to List Element to match with class name.

---
+++ ListElement.java
@@ -0,0 +1,30 @@
+/*
+*
+* Filename: ExampleList.java
+* Author: Noah Rowbotham (200364559)
+*
+*/
+
+public class ListElement {
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement() {
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+	
+	public static void main(String[], args) {
+		
+	}
+}
